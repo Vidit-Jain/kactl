2ea231f3f8545b4eb0d9443f474839bbc319b772
Vidit Jain <viditjain@live.com>
2023-04-01 10:41:55 +0530

    Switched to tabwidth=2 for data-structures

--- content/data-structures/DSU.h
+++ content/data-structures/DSU.h
@@ -9,15 +9,15 @@
 #pragma once
 
 struct DSU{
-    vi dsu;
-    int num_comps;
-    DSU(int n) : dsu(n, -1), num_comps(n) {}
-    int parent(int i){ return dsu[i] < 0 ? i : dsu[i] = parent(dsu[i]); }
-    int size(int i) { return -dsu[parent(i)]; }
-    int operator[](int i){ return parent(i); }
-    void unify(int a, int b){
-        a = parent(a), b = parent(b);
-        if(dsu[a] > dsu[b]) swap(a, b);
-        if(a!=b) dsu[a] += dsu[b], dsu[b] = a, num_comps--;
-    }
+	vi dsu;
+	int num_comps;
+	DSU(int n) : dsu(n, -1), num_comps(n) {}
+	int parent(int i){ return dsu[i] < 0 ? i : dsu[i] = parent(dsu[i]); }
+	int size(int i) { return -dsu[parent(i)]; }
+	int operator[](int i){ return parent(i); }
+	void unify(int a, int b){
+		a = parent(a), b = parent(b);
+		if(dsu[a] > dsu[b]) swap(a, b);
+		if(a!=b) dsu[a] += dsu[b], dsu[b] = a, num_comps--;
+	}
 };
