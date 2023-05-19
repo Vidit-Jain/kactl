44c38461c516aa11d35c7631034e53f13a4398c5
akcube <a.kishorekumar@students.iiit.ac.in>
2023-05-19 11:55:29 +0530

    Added trie, [UNTESTED]

---
+++ content/strings/Trie.h
@@ -0,0 +1,28 @@
+/**
+ * Author: A Kishore Kumar
+ * Date: 2023-05-19
+ * Source: Myself
+ * ExtDesc: Simple integer pointer based trie. 
+ * Status: NOT-TESTED
+ */
+const int NX = int(1e6) + int(5e5);
+int arr[NX][26];
+int lastocc;
+void Trie(){
+    lastocc = 0;
+    memset(arr, 0, sizeof(arr));
+}
+void insert(const string &x){
+    int curptr = 0;
+    for(auto ch:x){
+        if(arr[curptr][ch-'a']==0) arr[curptr][ch-'a'] = ++lastocc;
+        curptr = arr[curptr][ch-'a'];
+    }
+}
+int search(const string &x){
+    int curptr = 0;
+    for(auto ch:x)
+        if(arr[curptr][ch-'a']==0) return 0;
+        else curptr = arr[curptr][ch-'a'];
+    return 1;
+}
