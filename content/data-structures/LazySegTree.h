ecd5ef6b0493abe89f3e20253824fc820efe9f1a
Vidit Jain <viditjain@live.com>
2023-05-19 15:11:26 +0530

    Change lazy segtree

--- content/data-structures/LazySegTree.h
+++ content/data-structures/LazySegTree.h
@@ -10,53 +10,48 @@
  */
 
 template<typename T, typename L>
-struct LazySegmentTree {
-	int n, h;
-	T m_id; vector<T> t;
-	L l_id; vector<L> l;
+struct LazySegtree {
+    signed n;
+    vector<T> t; vector<L> l;    
+    T id; L l_id;
+ 
+    int bc(int n) { return (n == 1) ? 1 : (1 << (32 - __builtin_clz (n-1))); };
+    T f(T a, T b) { return max(a, b); } 
+    T map(T v, L val, int len) { return v+val; }
+    void compose(L &cl, L &ul, int len){ cl += ul; }
 
-	T f(T a, T b) { return max(a, b); }
-	T map(T v, L val, int len) { return v+val; }
-	void compose(L &cl, L &ul, int len){ cl += ul; }
+    LazySegtree(const vector<T> &a, T id, L l_id) : n(bc(sz(a))), id(id), l_id(l_id), t(2*n), l(2*n){
+        for (int i = 0; i < sz(a); i++) t[i + n] = a[i]; 
+        for (int i = n - 1; i >= 1; i--) t[i] = f(t[2 * i], t[2 * i + 1]);
+    }
+ 
+    void apply(int v, L upd, int len) {
+        t[v] = map(t[v], upd, len);
+        if (v < n) compose(l[v], upd, len);
+    }
 
-	void apply(int v, L upd, int len){
-		t[v] = map(t[v], upd, len);
-		if(v < n) compose(l[v], upd, len);
-	}
-	LazySegmentTree(const vector<T> &a, T m_id, L l_id) : n(sz(a)), 
-			h(32-__builtin_clz(signed(n))), m_id(m_id), t(2*n),
-			l_id(l_id), l(n, l_id){
-		for(int i=0; i < n; i++) t[n+i] = a[i];
-		for(int i=n-1; i >= 1; i--)
-			t[i] = f(t[2*i], t[2*i+1]);
-	}
-	void pull(int v){
-		for(int p=v>>1, k=2; p >= 1; p>>=1, k<<=1)
-			t[p] = map(f(t[2*p], t[2*p+1]), l[p], k);
-	}
-	void push(int v){
-		for(int lvl=h,p=(v>>lvl); lvl >= 1; lvl--,p=(v>>lvl)){
-			int k = 1 << (lvl-1);
-			apply(2*p, l[p], k), apply(2*p+1, l[p], k);
-			l[p] = l_id;
-		}
-	}
-	template <class B> void process(int l, int r, int msk, B op){
-		int l0=l+n, r0=r+n, k=1; assert(l <= r);
-		if(msk & (1 << 0)) push(l0), push(r0);
-		for(l += n, r += n; l <= r; l>>=1, r>>=1, k<<=1) {
-			if(l == r) { op(l, k); break; }
-			if(l&1) op(l++, k);
-			if(!(r&1)) op(r--, k);
-		}
-		if(msk & (1 << 1)) pull(l0), pull(r0);
-	}
-	void update(int lr, int rr, L val){ // Commutatvie upd => msk=0b10
-		process(lr, rr, 0b11, [&](int v, int k){ apply(v, val, k); });
-	}
-	T query(int lr, int rr){ // Does not work with commutative functions
-		T ret = m_id; 
-		process(lr, rr, 0b1, [&](int v, int k){ ret = f(ret, t[v]); }); 
-		return ret;
-	}
+    void push(int v) {
+        apply(2*v, l[v], -1); apply(2*v+1, l[v], -1);
+        l[v] = l_id;
+    }
+    T query(int node, int l, int r, int ql, int qr, int upd, int v) {
+        if (r < ql or qr < l) return id;
+        if (ql <= l and r <= qr) {
+            if (upd) apply(node, v, -1);
+            return t[node];
+        }
+        int m = l + (r - l) / 2;
+        push(node);
+        if (upd) {
+            query(2 * node, l, m, ql, qr, upd, v);
+            query(2 * node + 1, m + 1, r, ql, qr, upd, v);
+            t[node] = f(t[2 * node], t[2 * node + 1]);
+            return T{};
+        }
+        else
+            return f(query(2 * node, l, m, ql, qr, upd, v), query(2 * node + 1, m + 1, r, ql, qr, upd, v));
+    }
+
+    void update(int ul, int ur, int v) { query(1, 0, n - 1, ul, ur, 1, v);}
+    T query(int ql, int qr) { return query(1, 0, n - 1, ql, qr, 0, -1);}
 };
