da9eca25f829598da20e72be41179872fcef8b7d
akcube <a.kishorekumar@students.iiit.ac.in>
2023-05-19 11:55:48 +0530

    Modified Mo's on trees

--- content/data-structures/MoQueries.h
+++ content/data-structures/MoQueries.h
@@ -9,9 +9,9 @@
  */
 #pragma once
 
-void add(int ind, int end) { ... } // add a[ind] (end = 0 or 1)
-void del(int ind, int end) { ... } // remove a[ind]
-int calc() { ... } // compute current answer
+auto add = [&](int ind, int end) {};
+auto del = [&](int ind, int end) {};
+auto calc = [&]() {};
 
 vi mo(vector<pii> Q) {
 	int L = 0, R = 0, blk = 350; // ~N/sqrt(Q)
@@ -30,31 +30,33 @@ vi mo(vector<pii> Q) {
 	return res;
 }
 
-vi moTree(vector<array<int, 2>> Q, vvi& ed, int root=0){
-	int N = sz(ed), pos[2] = {}, blk = 350; // ~N/sqrt(Q)
-	vi s(sz(Q)), res = s, I(N), L(N), R(N), in(N), par(N);
-	add(0, 0), in[0] = 1;
-	auto dfs = [&](int x, int p, int dep, auto& f) -> void {
-		par[x] = p;
-		L[x] = N;
-		if (dep) I[x] = N++;
-		for (int y : ed[x]) if (y != p) f(y, x, !dep, f);
-		if (!dep) I[x] = N++;
-		R[x] = N;
-	};
-	dfs(root, -1, 0, dfs);
-#define K(x) pii(I[x[0]] / blk, I[x[1]] ^ -(I[x[0]] / blk & 1))
-	iota(all(s), 0);
-	sort(all(s), [&](int s, int t){ return K(Q[s]) < K(Q[t]); });
-	for (int qi : s) rep(end,0,2) {
-		int &a = pos[end], b = Q[qi][end], i = 0;
-#define step(c) { if (in[c]) { del(a, end); in[a] = 0; } \
-                  else { add(c, end); in[c] = 1; } a = c; }
-		while (!(L[b] <= L[a] && R[a] <= R[b]))
-			I[i++] = b, b = par[b];
-		while (a != b) step(par[a]);
-		while (i--) step(I[i]);
-		if (end) res[qi] = calc();
-	}
-	return res;
-}
+vector<array<int,2>> Q(q);
+auto moTree = [&](int root=0){
+    int N = sz(adj), pos[2] = {}, blk = 350; // ~N/sqrt(Q)
+    vi s(sz(Q)), res = s, I(N), L(N), R(N), in(N), par(N);
+    add(0, 0), in[0] = 1;
+    function<void(int,int,int)> dfs = [&](int x, int p, int dep){
+        par[x] = p; L[x] = N;
+        if (dep) I[x] = N++;
+        for (auto &to : adj[x]) if (to != p) dfs(to, x, !dep);
+        if (!dep) I[x] = N++;
+        R[x] = N;
+    };
+    dfs(root, -1, 0);
+    auto K = [&](auto &x) { return pii(I[x[0]] / blk, I[x[1]] ^ -(I[x[0]] / blk & 1)); };
+    iota(all(s), 0);
+    sort(all(s), [&](int s, int t){ return K(Q[s]) < K(Q[t]); });
+    for (int qi : s) rep(end,0,2) {
+        int &a = pos[end], b = Q[qi][end], i = 0;
+        auto step = [&](int c){
+            if (in[c]) del(a, end), in[a] = 0;
+            else add(c, end), in[c] = 1;
+            a = c;};
+        while (!(L[b] <= L[a] && R[a] <= R[b]))
+            I[i++] = b, b = par[b];
+        while (a != b) step(par[a]);
+        while (i--) step(I[i]);
+        if (end) res[qi] = calc();
+    }
+    return res;
+};
