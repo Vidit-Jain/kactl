24bcb84e7930f2055c66f54ac8fc697db001c0ff
akcube <a.kishorekumar@students.iiit.ac.in>
2023-11-07 04:49:05 +0530

    Updated Trie code, UNTESTED

--- content/strings/Trie.h
+++ content/strings/Trie.h
@@ -5,24 +5,22 @@
  * ExtDesc: Simple integer pointer based trie. 
  * Status: NOT-TESTED
  */
-const int NX = int(1e6) + int(5e5);
-int arr[NX][26];
-int lastocc;
-void Trie(){
-    lastocc = 0;
-    memset(arr, 0, sizeof(arr));
-}
-void insert(const string &x){
-    int curptr = 0;
-    for(auto ch:x){
-        if(arr[curptr][ch-'a']==0) arr[curptr][ch-'a'] = ++lastocc;
-        curptr = arr[curptr][ch-'a'];
+template<int NX = int(1e6 + 5e5)>
+struct Trie {
+    int root, lastocc;
+    array<array<int, 26>, NX> mem; // use vector if stack storage dies
+    void insert(const string &s){
+        int curptr = root;
+        for(auto &c : s){
+            if(!mem[curptr][c - 'a']) mem[curptr][c - 'a'] = ++lastocc;
+            curptr = mem[curptr][c - 'a'];
+        }
     }
-}
-int search(const string &x){
-    int curptr = 0;
-    for(auto ch:x)
-        if(arr[curptr][ch-'a']==0) return 0;
-        else curptr = arr[curptr][ch-'a'];
-    return 1;
-}
+    bool search(const string &s){
+        int curptr = root;
+        for(auto &c : s)
+            if(!mem[curptr][c - 'a']) return false;
+            else curptr = mem[curptr][c - 'a'];
+        return true;
+    }
+};
